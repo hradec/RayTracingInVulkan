edf87b7f108f74502111db2de8b6b1a641e9438f
rhradec <me@hradec.com>
2021-01-15 17:47:17 -0800

    copy some libs from docker so we can try to run the built executable on any distro. Also creates a run.sh to make it easier to run!

--- build_linux_using_docker.sh
+++ build_linux_using_docker.sh
@@ -12,7 +12,7 @@ docker rm trow_away_ray_tracing_in_vulkan 2>&1 >/dev/null
 
 # build the docker image, which will build
 # this depot in Ubuntu.
-docker build . \
+docker inspect ray_tracing_in_vulkan >/dev/null || docker build . \
     -f docker/Dockerfile \
     -t ray_tracing_in_vulkan \
     --compress \
@@ -21,8 +21,15 @@ docker build . \
     --build-arg http="$http_proxy" \
     --build-arg https="$https_proxy" \
 \
-&& mkdir -p ./build_ubuntu20.04/ \
+&& mkdir -p ./build_ubuntu20.04/linux/libs/ \
 && docker create --name trow_away_ray_tracing_in_vulkan ray_tracing_in_vulkan \
 && docker cp trow_away_ray_tracing_in_vulkan:/root/depot/build/linux/ ./build_ubuntu20.04/ \
+&& docker cp trow_away_ray_tracing_in_vulkan:/lib/x86_64-linux-gnu/libm.so.6 ./build_ubuntu20.04/linux/libs/ \
+&& docker cp trow_away_ray_tracing_in_vulkan:/lib/x86_64-linux-gnu/libm-2.31.so ./build_ubuntu20.04/linux/libs/ \
+&& docker cp trow_away_ray_tracing_in_vulkan:/lib/x86_64-linux-gnu/libstdc++.so.6 ./build_ubuntu20.04/linux/libs/ \
+&& docker cp trow_away_ray_tracing_in_vulkan:/lib/x86_64-linux-gnu/libstdc++.so.6.0.28 ./build_ubuntu20.04/linux/libs/ \
 && docker rm trow_away_ray_tracing_in_vulkan \
-&& echo -e "\n\nSuscessfully build in ./build/ubuntu20.04/\n\n"
+&& echo -e "#!/bin/bash\n\ncd \$(dirname \$(readlink -f \$BASH_SOURCE))\nLD_LIBRARY_PATH=\$(pwd)/libs ./bin/RayTracer\n" > ./build_ubuntu20.04/linux/run.sh \
+&& chmod a+x ./build_ubuntu20.04/linux/run.sh \
+&& echo -e "\n\nSuscessfully build in ./build_ubuntu20.04/\n\n"
+ln -s build_ubuntu20.04/linux/run.sh  ./
