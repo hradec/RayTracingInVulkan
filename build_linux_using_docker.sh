f71f4e0b0e4f393ccdf886bd0c801e4986a5d443
Roberto Hradec <rhradec@vfxws-033.local>
2021-01-15 23:13:52 -0200

    added proxy support

--- build_linux_using_docker.sh
+++ build_linux_using_docker.sh
@@ -12,12 +12,14 @@ docker rm trow_away_ray_tracing_in_vulkan 2>&1 >/dev/null
 
 # build the docker image, which will build
 # this depot in Ubuntu.
-echo  docker build . \
+docker build . \
     -f docker/Dockerfile \
     -t ray_tracing_in_vulkan \
     --compress \
     --rm \
     --build-arg VK_VERSION=$VK_VERSION \
+    --build-arg http="$http_proxy" \
+    --build-arg https="$https_proxy" \
 \
 && mkdir -p ./build_ubuntu20.04/ \
 && docker create --name trow_away_ray_tracing_in_vulkan ray_tracing_in_vulkan \
